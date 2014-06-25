2fcedeb62f45471e751df104459d02dc4a7a20a2
Wendy <wendybeth010@gmail.com>
2014-06-25 00:15:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Creating a message board with the [*Railsbridge Intermediate Rails*](http://docs.railsbridge.org/intermediate-rails/intermediate-rails) curriculum. Lots of notes herein lie.
+
+Divided into challenges:
+* Create a new Rails app with a static home page
+* Install Devise
+* Make it pretty with bootstrap
+* Add pages to create and look at individual posts
+* Make a posts index page
+* Add replying
+* Inline replying on a post
+* Other features
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsChallenge::Application.config.secret_key_base = '9ee88bf96015576f7186679eee78b1ba0553e54014e0ff9d1b5ec1f16789252282beeedb943d3d8fad89921d7264920d5a24e61acd2d8eb797d9a2b9e9b3f749'
